0d8dd6e6815d0bdd29a27cab2c500f7e6668fe65
Raine Adams <rainedoescode@gmail.com>
2023-10-08 18:31:40 -0500

    Add tile types class

---
+++ tile_types.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+import numpy as np
+
+# Tile graphics structured type compatible with Console.tiles_rgb
+grapic_dt = np.dtype(
+    [
+        ("ch", np.int32), # Unicode codepoint
+        ("fg", "3B"), # 3 unsigned bytes, for RGB colors
+        ("bg", "3B"),
+    ]
+)
+
+# Tile struct used for statically defined tile data
+tile_dt = np.dtype(
+    [
+        ("walkable", np.bool), # True if tile can be walked over
+        ("transparent", np.bool), # True if tile doesn't block FOV
+        ("dark", grapic_dt) # Graphics for when tile not in FOV
+    ]
+)
+
+
+def new_tile(
+        *, # Enforce use of keywords so parameter order does not matter
+        walkable: int,
+        transparent: int,
+        dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+) -> np.ndarray:
+    """Helper function for defining individual tyle types"""
+    return np.array((walkable, transparent, dark), dtype=tile_dt)
+
+
+floor = new_tile(
+    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+)
+
+wall = new_tile(
+    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+)
