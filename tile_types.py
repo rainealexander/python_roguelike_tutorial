7c124d617b8d57847b964774bd35da82dca7a57e
Raine Adams <rainedoescode@gmail.com>
2023-10-09 17:04:42 -0500

    Tile color and character adjustments

--- tile_types.py
+++ tile_types.py
@@ -32,9 +32,9 @@ def new_tile(
 
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True, transparent=True, dark=(ord(" "), (65, 45, 75), (50, 45, 35)),
 )
 
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable=False, transparent=False, dark=(ord("█"), (85, 85, 75), (10, 10, 10)),
 )
