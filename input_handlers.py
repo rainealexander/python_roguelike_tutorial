1c13fb4935f519f30ba5dd7d06fd3d3953dab0d5
Raine Adams <rainedoescode@gmail.com>
2023-10-12 19:00:25 -0500

    Refactor input handling for movement, add wait keys

--- input_handlers.py
+++ input_handlers.py
@@ -5,10 +5,47 @@ import tcod
 
 import tcod.event
 
-from actions import Action, BumpAction, EscapeAction
+from actions import Action, BumpAction, EscapeAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
+
+
+MOVE_KEYS = {
+    # Arrow keys
+    tcod.event.KeySym.UP: (0, -1),
+    tcod.event.KeySym.DOWN: (0, 1),
+    tcod.event.KeySym.LEFT: (-1, 0),
+    tcod.event.KeySym.RIGHT: (1, 0),
+    tcod.event.KeySym.HOME: (-1, -1),
+    tcod.event.KeySym.END: (-1, 1),
+    tcod.event.KeySym.PAGEUP: (1, -1),
+    tcod.event.KeySym.PAGEDOWN: (1, 1),
+    # Numpad keys
+    tcod.event.KeySym.KP_8: (0, -1),
+    tcod.event.KeySym.KP_2: (0, 1),
+    tcod.event.KeySym.KP_4: (-1, 0),
+    tcod.event.KeySym.KP_6: (1, 0),
+    tcod.event.KeySym.KP_7: (-1, -1),
+    tcod.event.KeySym.KP_1: (-1, 1),
+    tcod.event.KeySym.KP_9: (1, -1),
+    tcod.event.KeySym.KP_3: (1, 1),
+    # Vi keys
+    tcod.event.KeySym.k: (0, -1),
+    tcod.event.KeySym.j: (0, 1),
+    tcod.event.KeySym.h: (-1, 0),
+    tcod.event.KeySym.l: (1, 0),
+    tcod.event.KeySym.y: (-1, -1),
+    tcod.event.KeySym.b: (-1, 1),
+    tcod.event.KeySym.u: (1, -1),
+    tcod.event.KeySym.n: (1, 1),
+}
+
+WAIT_KEYS = {
+    tcod.event.KeySym.PERIOD,
+    tcod.event.KeySym.KP_5,
+    tcod.event.KeySym.CLEAR,
+}
 
 
 class EventHandler(tcod.event.EventDispatch[Action]):
@@ -40,16 +77,11 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         player = self.engine.player
 
-        if key == tcod.event.KeySym.UP:
-            action = BumpAction(player, dx=0, dy=-1)
-        elif key == tcod.event.KeySym.DOWN:
-            action = BumpAction(player, dx=0, dy=1)
-        elif key == tcod.event.KeySym.LEFT:
-            action = BumpAction(player, dx=-1, dy=0)
-        elif key == tcod.event.KeySym.RIGHT:
-            action = BumpAction(player, dx=1, dy=0)
-
-        
+        if key in MOVE_KEYS:
+            dx, dy = MOVE_KEYS[key]
+            action = BumpAction(player, dx, dy)
+        elif key in WAIT_KEYS:
+            action = WaitAction(player)
         elif key == tcod.event.KeySym.ESCAPE:
             action = EscapeAction(player)
 
