622df57ec9dd29e158c3b2c305526cd76a9daadd
Raine Adams <rainedoescode@gmail.com>
2023-10-18 11:07:29 -0500

    Implement basic game-over event

--- input_handlers.py
+++ input_handlers.py
@@ -52,7 +52,16 @@ class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
         self.engine = engine
 
+
+    def handle_events(self) -> None:
+        raise NotImplementedError()
     
+
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+
+class MainGameEventHandler(EventHandler):    
     def handle_events(self) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
@@ -64,10 +73,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
             self.engine.handle_enemy_turns()
             self.engine.update_fov() # Update FOV before player's next action
-
-
-    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
-        raise SystemExit()
     
 
     def ev_keydown(self, event:tcod.event.KeyDown) -> Optional[Action]:
@@ -85,5 +90,28 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         elif key == tcod.event.KeySym.ESCAPE:
             action = EscapeAction(player)
 
+        # No valid key press
+        return action
+
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        key = event.sym
+
+        if key == tcod.event.KeySym.ESCAPE:
+            action = EscapeAction(self.engine.player)
+        
         # No valid key press
         return action
